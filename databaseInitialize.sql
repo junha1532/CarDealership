6d5db68da28f96d68ef1d3961687977ad2cb7d2a
Junha Park <junha.park@mail.mcgill.ca>
2021-09-28 00:10:42 -0400

    222

--- databaseInitialize.sql
+++ databaseInitialize.sql
@@ -86,7 +86,6 @@ Create TABLE SALE(
 	SaleId INT NOT NULL AUTO_INCREMENT,
 	Primary key (SaleId),
     SpecialTitle varchar(20),
---     vehicleId int not null,
     salespersonId int not null,
     customerName varchar(20) NOT NULL,
 	customerEmail varchar(20) NOT NULL,
@@ -97,3 +96,26 @@ Create TABLE SALE(
     purchasePrice INT NOT NULL,
     purchaseType varChar(20) NOT NULL
 );
+
+INSERT INTO user(userFirstName, userLastName, userType, userEmail, password)
+Values("Junha", "Park", "ADMIN", "junha.park@cardealership.com", "12345ABC");
+
+INSERT INTO Make(userId, makeName, dateAdded) 
+Values(1, "Audi", "2021-09-01");
+
+INSERT INTO Make(userId, makeName, dateAdded) 
+Values(1, "Mercedes", "2021-09-10");
+
+INSERT INTO Make(userId, makeName, dateAdded) 
+Values(1, "Ferrarri", "2021-09-14");
+
+INSERT INTO MODEL(MakeId, userId, modelName, dateAdded) 
+Values(1, 1, "Audi123", "2021-09-14");
+
+INSERT INTO vehicle(userId, VIN, Year, MakeId, ModelID, Color, Interior, BodyStyle, Transmission, Mileage, SalePrice, MSRP, featured, dateAdded, DESCRIPTION)
+Values(1, "01230AJKNDSD", 2020, 1, 1, "Mat Black", "Grey", "SUV", "Auto", 20000, 35000, 40000, false, "2021-09-14", "COOL CAR. MUST BUY!");
+
+DELETE FROM vehicle WHERE vehicleId = 1; 
+
+INSERT INTO sale(salesPersonId,customerName,customerEmail, customerAddress,customerAddress2,customerCity,customerZipCode,purchasePrice,purchaseType )
+Values(1, "Aubrey", "aubrey@abc.com", "Somewhere in US", "", "New York", "ZIP12", 35000, "CASH");
